6401c9caee06de208ecd1858c98a75c1bb19f5a2
Caio R. Vieira <caio.robs98@gmail.com>
2021-07-28 19:06:02 -0300

    ex 8 check

---
+++ 01-fundamentos/04bloco-javascript-intro/01dia-primeiros-passos/umPar.js
@@ -0,0 +1,10 @@
+const num1 = 11,
+	num2 = 3,
+	num3 = 265
+let atLeastOneEven = false;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+	atLeastOneEven = true;
+}
+
+return console.log(atLeastOneEven);
