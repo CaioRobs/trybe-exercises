4e1fcf5f82975c0178352082351656af63fd78ab
Caio R. Vieira <caio.robs98@gmail.com>
2021-07-28 18:33:59 -0300

    ex 1 to 5 check

---
+++ 01-fundamentos/04bloco-javascript-intro/01dia-primeiros-passos/maior.js
@@ -0,0 +1,12 @@
+// maior dos dois
+let a = 20,
+	b = 20,
+	maior = null;
+
+if (a >= b) {
+	maior = a
+} else if (b > a) {
+	maior = b
+} 
+
+console.log(maior)
---
+++ 01-fundamentos/04bloco-javascript-intro/01dia-primeiros-passos/negativo-positivo.js
@@ -0,0 +1,12 @@
+const numero = 0;
+let isPositive = null
+
+if (numero > 0) {
+	isPositive = true
+} else if (numero < 0) {
+	isPositive = false
+} else {
+	isPositive = 0
+}
+
+console.log(isPositive)
