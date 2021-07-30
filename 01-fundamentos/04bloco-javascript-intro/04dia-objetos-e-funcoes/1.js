5dfc35712f8fe6ea47ba605cb94361982a4b39c0
Caio R. Vieira <caio.robs98@gmail.com>
2021-07-30 19:21:28 -0300

    exercices day 4.4 check

--- 01-fundamentos/04bloco-javascript-intro/04dia-objetos-e-funcoes/1.js
+++ 01-fundamentos/04bloco-javascript-intro/04dia-objetos-e-funcoes/1.js
@@ -4,17 +4,17 @@ let info = {
 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-// console.log(`Bem vinda, ${info.personagem}`);
+console.log(`Bem vinda, ${info.personagem}`);
 
 info.recorrente = 'sim';
 console.log(info)
 
-// for (const key in info) {
-// 	console.log(key);
-// }
-// for (const key in info) {
-// 	console.log(info[key]);
-// }
+for (const key in info) {
+	console.log(key);
+}
+for (const key in info) {
+	console.log(info[key]);
+}
 
 let info2 = {
 	personagem: 'Tio Patinhas',
---
+++ 01-fundamentos/04bloco-javascript-intro/04dia-objetos-e-funcoes/2.js
@@ -0,0 +1,95 @@
+//1-
+function verificaPalindromo(string) {
+	let ehPalindromo = false;
+	const array = Array.from(string);
+
+	for (i = 0; i < array.length; i += 1) {
+		if (array[i] === array[(array.length - 1) - i]) {
+			ehPalindromo = true;
+		}
+
+		if (ehPalindromo === false) {
+			break;
+		}
+	}
+
+	if (ehPalindromo === true) {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log(verificaPalindromo('desenvolvimento'))
+
+//2-
+function higherNumberIndex(array) {
+	let maior = -Infinity;
+
+	for (const number of array) {
+		if (number >= maior) {
+			maior = number;
+		}
+	}
+	return array.indexOf(maior);
+}
+const test = [2, 3, 6, 7, 10, 0, -3];
+console.log(higherNumberIndex(test));
+
+//3-
+function smallestNumberIndex(array) {
+	let menor = Infinity;
+
+	for (const number of array) {
+		if (number <= menor) {
+			menor = number;
+		}
+	}
+	return array.indexOf(menor);
+}
+console.log(smallestNumberIndex(test));
+
+//4-
+function biggestNameIndex(nomes) {
+	let biggestIndex = 0;
+
+	for (let index = 0; index < nomes.length; index += 1) {
+		if (nomes[index].length >= nomes[biggestIndex].length) {
+			biggestIndex = index;
+		}
+	}
+	return biggestIndex;
+}
+const teste2 = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log(teste2[biggestNameIndex(teste2)])
+
+//5-
+function moreRepeats(array) {
+	let arrayRepeticoes = [];
+
+	for (let index = 0; index < array.length; index += 1) {
+		let repeticoesNumeroAtual = 0;
+
+		for (let index2 = 0; index2 < array.length; index2 += 1) {
+			if (array[index] === array[index2]) {
+				repeticoesNumeroAtual += 1;
+			}
+		}
+		arrayRepeticoes.push(repeticoesNumeroAtual);
+	}
+	const indexMaisRepete = higherNumberIndex(arrayRepeticoes);
+	return array[indexMaisRepete]
+}
+const teste3 = [2, 3, 2, 5, 8, 2, 3];
+console.log(moreRepeats(teste3))
+
+//6-
+function somatoriaAteN(numero) {
+	let soma = 0;
+	for (i = 1; i <= numero; i += 1) {
+		soma += i;
+	}
+	return soma;
+}
+console.log(somatoriaAteN(5))
+
+//7-
