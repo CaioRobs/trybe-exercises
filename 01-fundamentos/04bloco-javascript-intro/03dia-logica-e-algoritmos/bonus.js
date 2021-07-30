56a5035770fb4b3eb59664da04b5ee37af3eba5c
Caio R. Vieira <caio.robs98@gmail.com>
2021-07-30 00:19:00 -0300

    bonus 4.3

---
+++ 01-fundamentos/04bloco-javascript-intro/03dia-logica-e-algoritmos/bonus.js
@@ -0,0 +1,44 @@
+const n = 101;
+
+
+for (let index = 1; index <= n; index += 2) {
+	let asteriscos = '';
+	if (index === 1) {
+		for (let index2 = 1; index2 <= ((n - index) / 2); index2 += 1) {
+			asteriscos += ' ';
+		}
+
+		for (let index2 = 1; index2 <= index; index2 += 1) {
+			asteriscos += '*';
+		}
+
+		for (let index2 = 1; index2 <= ((n - index) / 2); index2 += 1) {
+			asteriscos += ' ';
+		}
+
+	} else if (index !== n) {
+		for (let index2 = 1; index2 <= ((n - index) / 2); index2 += 1) {
+			asteriscos += ' ';
+		}
+
+		asteriscos += '*';
+
+		for (let index2 = 1; index2 <= (index - 2); index2 += 1) {
+			asteriscos += ' ';
+		}
+
+		asteriscos += '*';
+
+		for (let index2 = 1; index2 <= ((n - index) / 2); index2 += 1) {
+			asteriscos += ' ';
+		}
+	} else {
+		for (index2 = 1; index2 <= n; index2 += 1) {
+			asteriscos += '*'
+		}
+	}
+	console.log(asteriscos);
+}
+//asteriscos => ''
+//index => 1
+//index2 => 1
