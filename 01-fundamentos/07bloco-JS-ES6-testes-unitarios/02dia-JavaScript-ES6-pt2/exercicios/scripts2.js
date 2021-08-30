d0eac6b76ca65411584dfefee48bdf44c969a167
Caio R. Vieira <caio.robs98@gmail.com>
2021-08-30 19:09:02 -0300

    termina exercicios do dia 7.2

--- 01-fundamentos/07bloco-JS-ES6-testes-unitarios/02dia-JavaScript-ES6-pt2/exercicios/scripts2.js
+++ 01-fundamentos/07bloco-JS-ES6-testes-unitarios/02dia-JavaScript-ES6-pt2/exercicios/scripts2.js
@@ -17,3 +17,61 @@ const lesson3 = {
 	professor: 'Maria Clara',
 	turno: 'noite',
 };
+
+const addKeyValue = (object, key, value) => object[key] = value;
+addKeyValue(lesson2, 'turno', 'noite');
+
+const listKeys = object => Object.keys(object);
+
+const getObjectSize = object => Object.keys(object).length;
+
+const listValues = object => Object.values(object);
+
+const allLessons = Object.assign({}, {
+	lesson1,
+	lesson2,
+	lesson3
+});
+// console.log(allLessons);
+
+const somaAlunos = object => {
+	let totalStudents = 0;
+	for (const lesson in object) {
+		if (Object.hasOwnProperty.call(object, lesson)) {
+			totalStudents += object[lesson].numeroEstudantes;
+		}
+	}
+	return totalStudents;
+}
+// console.log(somaAlunos(allLessons));
+
+const getValueByNumber = (lesson, number) => Object.values(lesson)[number];
+// console.log(getValueByNumber(lesson1, 0));
+
+const verifyPair = (lesson, key, value) => {
+	const entries = Object.entries(lesson);
+	for (const entry of entries) {
+		if (entry[0] === key && entry[1] === value) {
+			return true;
+		}
+	}
+	return false
+}
+// console.log(verifyPair(lesson3, 'turno', 'noite'));
+// console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+
+
+const countMathStudents = allLessons => {
+	studentsAmount = 0;
+	const lessons = Object.keys(allLessons)
+	for (index in lessons) {
+		if (allLessons[lessons[index]].materia === `Matemática`) {
+			studentsAmount += allLessons[lessons[index]].numeroEstudantes;
+		}
+	}
+
+	return studentsAmount;
+}
+
+console.log(countMathStudents(allLessons));
+console.log(Object.keys(allLessons));
