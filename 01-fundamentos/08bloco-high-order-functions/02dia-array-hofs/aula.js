5083e1728cf0b701ab2e475422960ef3f1f942f2
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-03 17:09:32 -0300

    aula do dia 8.1

---
+++ 01-fundamentos/08bloco-high-order-functions/02dia-array-hofs/aula.js
@@ -0,0 +1,41 @@
+const students = [{
+    name: 'Maria',
+    grade: 70,
+    approved: ''
+  },
+  {
+    name: 'José',
+    grade: 56,
+    approved: ''
+  },
+  {
+    name: 'Roberto',
+    grade: 90,
+    approved: ''
+  },
+  {
+    name: 'Ana',
+    grade: 81,
+    approved: ''
+  },
+];
+
+function verifyGrades() {
+  students.forEach((student) => {
+    if (student.grade >= 60) {
+      student.approved = 'Aprovado';
+    } else {
+      student.approved = 'Recuperação';
+    }
+  });
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
