064ed1d56a92c5b3837bcb16515031f0507b665e
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-08 20:12:52 -0300

    adiciona versao mais legivel do exercicio 7

--- 01-fundamentos/08bloco-high-order-functions/03dia-map-e-filter/exercicios/ex7.js
+++ 01-fundamentos/08bloco-high-order-functions/03dia-map-e-filter/exercicios/ex7.js
@@ -64,6 +64,15 @@ const books = [{
 
 const expectedResult = 'O Senhor dos Anéis';
 
-const authorWith3DotsOnName = () => books.find(book => book.author.name.split(' ').filter(word => word.endsWith('.')).length === 3).name
+const authorWith3DotsOnName = () => {
+  const bookBy3dotedAuthor = books.find(book => {
+    const authorNameArray = book.author.name.split(' ')
+    const endedWithDotOnAuthorName = authorNameArray.filter(word => word.endsWith('.'))
+    if (endedWithDotOnAuthorName.length === 3) return true;
+  });
+  return bookBy3dotedAuthor.name;
+}
+// abaixo a versão mais compacta desse codigo:
+// const authorWith3DotsOnName = () => books.find(book => book.author.name.split(' ').filter(word => word.endsWith('.')).length === 3).name
 
 assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
