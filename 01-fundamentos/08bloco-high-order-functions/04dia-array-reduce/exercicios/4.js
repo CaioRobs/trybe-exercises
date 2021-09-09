de5a69a5929e13918927c1b0a8eeeeb00c4665a8
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-09 18:54:15 -0300

    exercicio 4 ok

--- 01-fundamentos/08bloco-high-order-functions/04dia-array-reduce/exercicios/4.js
+++ 01-fundamentos/08bloco-high-order-functions/04dia-array-reduce/exercicios/4.js
@@ -63,3 +63,17 @@ const books = [{
 ];
 
 // Adicione o código do exercício aqui:
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+const longestNamedBook = () => books.reduce((acc, book) => acc.name.length > book.name.length ? acc : book);
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
