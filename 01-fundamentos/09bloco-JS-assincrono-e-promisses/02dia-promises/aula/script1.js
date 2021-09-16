f988a2c0f19e47b17b51525b8ec186f314bf1d85
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-16 15:54:54 -0300

    termina aula do dia 9.2

--- 01-fundamentos/09bloco-JS-assincrono-e-promisses/02dia-promises/aula/script1.js
+++ 01-fundamentos/09bloco-JS-assincrono-e-promisses/02dia-promises/aula/script1.js
@@ -1,1 +1,17 @@
 // wget 'https://pokeapi.co/api/v2/pokemon/ditto' - O - -q
+const promise = new Promise((resolve, reject) => {
+    const number = Math.floor(Math.random() * 11);
+
+    if (number <= 5) {
+      return reject(number);
+    }
+    resolve(number);
+  })
+  .then(number => `Que sucesso =) nosso número foi ${number}`)
+  .then(msg => console.log(msg))
+  .catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
+
+console.log(promise);
+setTimeout(() => {
+  console.log(promise);
+}, 1000);
