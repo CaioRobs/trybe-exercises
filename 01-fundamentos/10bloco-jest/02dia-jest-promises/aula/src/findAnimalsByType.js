0a70c6ff77cf9cb4816eddc3a987ee173c90172a
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-22 16:17:28 -0300

    termina aula do dia 10.2

---
+++ 01-fundamentos/10bloco-jest/02dia-jest-promises/aula/src/findAnimalsByType.js
@@ -0,0 +1,32 @@
+const animals = [{
+    name: 'Dorminhoco',
+    age: 1,
+    type: 'Dog'
+  },
+  {
+    name: 'Soneca',
+    age: 2,
+    type: 'Dog'
+  },
+  {
+    name: 'Preguiça',
+    age: 5,
+    type: 'Cat'
+  },
+];
+
+const findAnimalsByType = (type) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = animals.filter((animal) => animal.type === type);
+
+      if (arrayAnimals.length !== 0) {
+        return resolve(arrayAnimals);
+      }
+
+      return reject(new Error('Não possui esse tipo de animal.'));
+    }, 100);
+  })
+);
+
+module.exports = findAnimalsByType;
--- 01-fundamentos/10bloco-jest/02dia-jest-promises/aula/tests/segundoExemplo.test.js
+++ 01-fundamentos/10bloco-jest/02dia-jest-promises/aula/tests/segundoExemplo.test.js
@@ -1,6 +1,6 @@
 const asyncSum = (a, b, callback) => {
   setTimeout(() => {
-    const result = a + b + 1;
+    const result = a + b;
     callback(result);
   }, 500);
 };
