3a88377161ed728be06b27468dbf70f379f74b86
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-23 14:41:04 -0300

    inicia npm, instala o jest e separa diretorios de teste e sources

---
+++ 01-fundamentos/10bloco-jest/03dia-jest-mocking/aula/test/service.test.js
@@ -0,0 +1,7 @@
+const service = require('../src/service');
+
+test("#randomRgbColor", () => {
+  // testando se a função foi chamada
+  service.randomRgbColor();
+  expect(service.randomRgbColor).toHaveBeenCalled();
+});
