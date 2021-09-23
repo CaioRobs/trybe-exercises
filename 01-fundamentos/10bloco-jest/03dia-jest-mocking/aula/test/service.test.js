003fcf4e7519a40c01fd09c50051ff9e3ae95c3a
Caio R. Vieira <caio.robs98@gmail.com>
2021-09-23 14:51:05 -0300

    termina primeiro exemplo, jest.fn() e mockReturnValue()

--- 01-fundamentos/10bloco-jest/03dia-jest-mocking/aula/test/service.test.js
+++ 01-fundamentos/10bloco-jest/03dia-jest-mocking/aula/test/service.test.js
@@ -1,7 +1,21 @@
 const service = require('../src/service');
 
 test("#randomRgbColor", () => {
+  service.randomRgbColor = jest
+    .fn()
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
+        
   // testando se a função foi chamada
-  service.randomRgbColor();
-  expect(service.randomRgbColor).toHaveBeenCalled();
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(0);
+  
+  expect(service.randomRgbColor()).toBe('first call');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(1);
+
+  expect(service.randomRgbColor()).toBe('second call');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(2);
+
+  expect(service.randomRgbColor()).toBe('default value');
+  expect(service.randomRgbColor).toHaveBeenCalledTimes(3);
 });
