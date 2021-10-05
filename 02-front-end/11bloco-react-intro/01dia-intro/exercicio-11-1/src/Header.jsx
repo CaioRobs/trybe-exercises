20c0177e4774c3f489931355df6aa0c5a80e2228
Caio R. R. Vieira <84696473+CaioRobs@users.noreply.github.com>
2021-10-05 16:16:15 -0300

    Update 02-front-end/11bloco-react-intro/01dia-intro/exercicio-11-1/src/Header.jsx
    Co-authored-by: Erickson Siqueira da Silva <83774878+EricksonSiqueira@users.noreply.github.com>

--- 02-front-end/11bloco-react-intro/01dia-intro/exercicio-11-1/src/Header.jsx
+++ 02-front-end/11bloco-react-intro/01dia-intro/exercicio-11-1/src/Header.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <div className='App-header'>
+      <header className='App-header'>
         <h1>Conteúdos de Front-End</h1>
-      </div>
+      </header>
     )
   }
 };
