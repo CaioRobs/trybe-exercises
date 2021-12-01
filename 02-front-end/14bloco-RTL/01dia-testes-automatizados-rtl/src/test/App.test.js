2b1b7afe16d106882efd8974d53c0fde34291a47
Caio R. Vieira <caio.robs98@gmail.com>
2021-12-01 18:15:22 -0300

    termina dia

--- 02-front-end/14bloco-RTL/01dia-testes-automatizados-rtl/src/test/App.test.js
+++ 02-front-end/14bloco-RTL/01dia-testes-automatizados-rtl/src/test/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('Testando a aplicação, testando input', () => {
-  const { getByLabelText, getByText } = render(<App />)
-  const inputTask = getByLabelText('Tarefa:');
-  const labelTask = getByText('Tarefa:');
+  render(<App />)
+  const inputTask = screen.getByLabelText('Tarefa:');
+  const labelTask = screen.getByText('Tarefa:');
   test('Verificando se o label e o input existem no documento', () => {
     expect(labelTask).toBeInTheDocument();
     expect(inputTask).toBeInTheDocument();
@@ -15,3 +15,12 @@ describe('Testando a aplicação, testando input', () => {
     expect(inputTask.type).toBe('text');
   });
 });
+
+describe('Testando o Botao', () => {
+  render(<App />)
+  const addButton = screen.getByRole('button', {value: 'Adicionar'});
+  it('Existe um botão, com o texto "Adicionar"', () => {
+    expect(addButton).toBeInTheDocument();
+    expect(addButton).toBe('Adicionar');
+  })
+})
